e9090dd428210a3fe6aec522f0109382bd14d643
Anjalivibrant <anjali90wr@gmail.com>
2025-04-06 12:23:18 +0530

    removing the testing stage

--- check.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyA8MC0w5P11wIGeO3JeI66jdkSq2xBu2B0")
-models = genai.list_models()
-
-for model in models:
-    print(model.name)
